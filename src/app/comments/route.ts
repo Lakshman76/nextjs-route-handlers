da69b8e70f63849b082ac202bfc4b3bc79e1afea
Lakshman76 <lakshmankumar2603@gmail.com>
2024-10-31 12:45:40 +0530

    Handling POST request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,4 +1,25 @@
 import { comments } from "./data";
 export async function GET() {
-    return new Response(JSON.stringify(comments));
+    return new Response(JSON.stringify(comments), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
+
+// POST
+export async function POST(req: Request) {
+    const comment = await req.json();
+    const newComment = {
+        id: comments.length + 1,
+        text: comment.text,
+    };
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        status: 201,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 }
