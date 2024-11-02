48c91c8ae80331c4f213b3cb7859588121d57ba6
Lakshman76 <lakshmankumar2603@gmail.com>
2024-11-02 14:38:30 +0530

    setting and getting headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
+import { headers } from "next/headers";
+
 export async function GET() {
-    return new Response("profile API data!");
+    const headersList = headers();
+    console.log((await headersList).get("Connection")); // keep-alive
+    return new Response("<h1>profile API data! </h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        }
+    });
 }
