713b863b3a4b30085a66069f7fe6dc05d0499d7d
Lakshman76 <lakshmankumar2603@gmail.com>
2024-11-02 14:56:59 +0530

    get and set cookie

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,11 +1,18 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET() {
     const headersList = headers();
     console.log((await headersList).get("Connection")); // keep-alive
+
+    // set and get cookie
+    const cookiesList = await cookies();
+    cookiesList.set("resultsPerPage", "20");
+    console.log(cookiesList.get("resultsPerPage")); // { name: 'resultsPerPage', value: '20', path: '/' }
+
     return new Response("<h1>profile API data! </h1>", {
         headers: {
             "Content-Type": "text/html",
+            "set-cookie": "theme=dark",
         }
     });
 }
